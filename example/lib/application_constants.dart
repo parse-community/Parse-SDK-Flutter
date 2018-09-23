595636846951f9db5bf60298a90fa7ab2eccee0b
Phill Wiggins <phill.wiggins@gmail.com>
2018-09-23 10:06:42 +0100

    Update application_constants.dart

--- example/lib/application_constants.dart
+++ example/lib/application_constants.dart
@@ -1,5 +1,5 @@
 abstract class ApplicationConstants {
-  static const String PARSE_APPLICATION_ID = "bodycaldb";
-  static const String PARSE_MASTER_KEY = "343gf35g4t6hev445f4t5f45g45d";
-  static const String PARSE_SERVER_URL = "https://bc-server.purewowstudio.com";
+  static const String PARSE_APPLICATION_ID = "APP_ID";
+  static const String PARSE_MASTER_KEY = "MASTER_KEY";
+  static const String PARSE_SERVER_URL = "SERVER_URL";
 }
