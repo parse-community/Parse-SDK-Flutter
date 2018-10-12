57b9011617b13015f0219d382797a421a5c4d99a
Phill Wiggins <phill.wiggins@gmail.com>
2018-10-12 15:32:16 +0100

    Tidy test code

--- lib/network/parse_query.dart
+++ lib/network/parse_query.dart
@@ -15,7 +15,8 @@ class QueryBuilder implements ParseBaseObject {
   String field;
   Map results;
   Map constraint;
-  Map<String, Map<String, String>> whereMap = Map<String, Map<String, String>>();
+  Map<String, Map<String, String>> whereMap =
+      Map<String, Map<String, String>>();
 
   // QueryParams
   List<dynamic> equals;
@@ -58,31 +59,34 @@ class QueryBuilder implements ParseBaseObject {
   }
 
   String _buildQuery() {
-
     var existsMap = Map<String, String>();
 
     if (equals != null) existsMap = _runThroughQueryParams(equals, field);
-    if (containedIn != null) existsMap = _runThroughQueryParamsWithName(containedIn, "in", field);
-    if (regEx != null) existsMap = _runThroughQueryParamsWithName(regEx, "regex", field);
-    if (greaterThan != null) existsMap = _runThroughQueryParamsWithName(greaterThan, "gt", field);
-    if (contains != null) existsMap = _runThroughQueryParamsWithSearchTerms(contains, "term", field);
+    if (containedIn != null)
+      existsMap = _runThroughQueryParamsWithName(containedIn, "in", field);
+    if (regEx != null)
+      existsMap = _runThroughQueryParamsWithName(regEx, "regex", field);
+    if (greaterThan != null)
+      existsMap = _runThroughQueryParamsWithName(greaterThan, "gt", field);
+    if (contains != null)
+      existsMap =
+          _runThroughQueryParamsWithSearchTerms(contains, "term", field);
 
     //String query = r"""where={"Name":{"$text":{"$search":{"$term":"Diet"}}}}""";
-    String query = """where=${JsonEncoder().convert(existsMap).replaceAll(new RegExp(r'/'), '')}""";
-    Uri uri = Uri.parse(query);
+    String query = "where=${JsonEncoder().convert(existsMap)}";
 
     if (limit != 0) query += '?limit=$limit';
     if (skip != 0) query += '?skip=$skip';
 
-      return query;
+    return query;
   }
 
-  Map<String, String> _runThroughQueryParams(List<dynamic> list, String queryParam) {
+  Map<String, String> _runThroughQueryParams(
+      List<dynamic> list, String queryParam) {
     Map<String, String> mapToReturn = Map<String, String>();
     var params = "";
 
     if (list.isNotEmpty) {
-
       if (list.length == 1) {
         params = list[0];
       } else {
@@ -92,7 +96,6 @@ class QueryBuilder implements ParseBaseObject {
 
         params.substring(0, params.length - 2);
       }
-
     }
 
     mapToReturn[queryParam] = params;
@@ -100,12 +103,12 @@ class QueryBuilder implements ParseBaseObject {
     return mapToReturn;
   }
 
-  Map<String, String> _runThroughQueryParamsWithName(List<dynamic> list, String queryParam, String fieldName) {
-
+  Map<String, String> _runThroughQueryParamsWithName(
+      List<dynamic> list, String queryParam, String fieldName) {
     Map<String, String> mapToReturn = Map<String, String>();
     Map<String, dynamic> mapWithParamData = Map<String, dynamic>();
 
-    for (var item in list){
+    for (var item in list) {
       mapWithParamData["\$$queryParam"] = item;
     }
 
@@ -116,14 +119,14 @@ class QueryBuilder implements ParseBaseObject {
     return mapToReturn;
   }
 
-  Map<String, String> _runThroughQueryParamsWithSearchTerms(List<dynamic> list, String queryParam, String fieldName) {
-
+  Map<String, String> _runThroughQueryParamsWithSearchTerms(
+      List<dynamic> list, String queryParam, String fieldName) {
     Map<String, String> mapToReturn = Map<String, String>();
     Map<String, dynamic> mapWithParamData = Map<String, dynamic>();
     Map<String, String> textEntry = Map<String, String>();
     Map<String, String> searchEntry = Map<String, String>();
 
-    for (var item in list){
+    for (var item in list) {
       mapWithParamData["\$$queryParam"] = item;
     }
 
