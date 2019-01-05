50a737e63ece92acd08882903f1de2e87ce92814
Phill <phill.wiggins@gmail.com>
2019-01-05 17:09:27 +0000

    Added ability to use less than and greater than in single query

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,8 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_plugin_example/application_constants.dart';
 import 'package:flutter_plugin_example/diet_plan.dart';
-import 'package:parse_server_sdk/objects/parse_object.dart';
 import 'package:parse_server_sdk/network/parse_query.dart';
-import 'package:parse_server_sdk/objects/parse_response.dart';
+import 'package:parse_server_sdk/objects/parse_object.dart';
 import 'package:parse_server_sdk/objects/parse_user.dart';
 import 'package:parse_server_sdk/parse.dart';
 
@@ -17,7 +14,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   void initState() {
     super.initState();
@@ -35,21 +31,19 @@ class _MyAppState extends State<MyApp> {
         body: new Center(
           child: new Text('Running Parse init'),
         ),
-        floatingActionButton: new FloatingActionButton(
-            onPressed: runTestQueries),
+        floatingActionButton:
+            new FloatingActionButton(onPressed: runTestQueries),
       ),
     );
   }
 
   initParse() async {
     // Initialize parse
-    Parse().initialize(
-        ApplicationConstants.PARSE_APPLICATION_ID,
+    Parse().initialize(ApplicationConstants.PARSE_APPLICATION_ID,
         ApplicationConstants.PARSE_SERVER_URL,
         masterKey: ApplicationConstants.PARSE_MASTER_KEY,
         appName: ApplicationConstants.APP_NAME,
-        debug: true
-    );
+        debug: true);
   }
 
   runTestQueries() {
@@ -86,7 +80,9 @@ class _MyAppState extends State<MyApp> {
     var response = await DietPlan().get('R5EonpUDWy');
 
     if (response.success) {
-      print(ApplicationConstants.APP_NAME + ": " + (response.result as DietPlan).toString());
+      print(ApplicationConstants.APP_NAME +
+          ": " +
+          (response.result as DietPlan).toString());
     } else {
       print(ApplicationConstants.APP_NAME + ": " + response.exception.message);
     }
@@ -96,19 +92,23 @@ class _MyAppState extends State<MyApp> {
     // Query for an object by name
     var queryBuilder = QueryBuilder<DietPlan>(DietPlan())
       ..field = DietPlan.FAT
-      ..notEqualTo = [60, 65];
+      ..greaterThan = [59]
+      ..lessThan = [61];
 
     var response = await queryBuilder.query();
 
     if (response.success) {
-      print(ApplicationConstants.APP_NAME + ": " + ((response.result as List<dynamic>).first as DietPlan).toString());
+      print(ApplicationConstants.APP_NAME +
+          ": " +
+          ((response.result as List<dynamic>).first as DietPlan).toString());
     } else {
       print(ApplicationConstants.APP_NAME + ": " + response.exception.message);
     }
   }
 
   initUser() async {
-    ParseUser().create("TestFlutter", "TestPassword123", "TestFlutterSDK@gmail.com");
+    ParseUser()
+        .create("TestFlutter", "TestPassword123", "TestFlutterSDK@gmail.com");
     var user = await ParseUser().signUp();
     user = await ParseUser().login();
     user = await ParseUser().currentUser(fromServer: true);
--- lib/network/parse_query.dart
+++ lib/network/parse_query.dart
@@ -50,46 +50,54 @@ class QueryBuilder <T extends ParseObject> {
   }
 
   String _buildQuery() {
-    var existsMap = Map();
+    var queries = List<String>();
 
     // START QUERY
-    String query = "where=";
+    const String QUERY_START = "where={";
+    const String QUERY_END = "}";
+
+    var query = QUERY_START;
 
     // ADD PARAM TO MAP
-
     //Needs fixing
-    if (equals != null) existsMap = _runThroughQueryParams(equals, field);
-    if (contains != null) existsMap = _runThroughQueryParamsWithName(contains, "\$term", field);
+    if (equals != null) queries.add(_runThroughQueryParams(equals, field));
+    if (contains != null) queries.add(_buildQueryWithOperatorAndField(contains, "\$term", field));
 
     // Works
-    if (lessThan != null) existsMap = _runThroughQueryParamsWithName(lessThan, "\$lt", field);
-    if (lessThanOrEqualTo != null) existsMap = _runThroughQueryParamsWithName(lessThanOrEqualTo, "\$lte", field);
-    if (greaterThan != null) existsMap = _runThroughQueryParamsWithName(greaterThan, "\$gt", field);
-    if (greaterThanOrEqualTo != null) existsMap = _runThroughQueryParamsWithName(greaterThanOrEqualTo, "\$gte", field);
-    if (notEqualTo != null) existsMap = _runThroughQueryParamsWithName(notEqualTo, "\$ne", field);
+    if (lessThan != null) queries.add(_buildQueryWithOperatorAndField(lessThan, "\$lt", field));
+    if (lessThanOrEqualTo != null) queries.add(_buildQueryWithOperatorAndField(lessThanOrEqualTo, "\$lte", field));
+    if (greaterThan != null) queries.add(_buildQueryWithOperatorAndField(greaterThan, "\$gt", field));
+    if (greaterThanOrEqualTo != null) queries.add(_buildQueryWithOperatorAndField(greaterThanOrEqualTo, "\$gte", field));
+    if (notEqualTo != null) queries.add(_buildQueryWithOperatorAndField(notEqualTo, "\$ne", field));
 
     // Not sure
-    if (containedIn != null) existsMap = _runThroughQueryParamsWithName(containedIn, "\$in", field);
-    if (notContainedIn != null) existsMap = _runThroughQueryParamsWithName(notContainedIn, "\$nin", field);
-    if (exists != null) existsMap = _runThroughQueryParamsWithName(exists, "\$exists", field);
-    if (select != null) existsMap = _runThroughQueryParamsWithName(select, "\$select", field);
-    if (dontSelect != null) existsMap = _runThroughQueryParamsWithName(dontSelect, "\$dontSelect", field);
-    if (all != null) existsMap = _runThroughQueryParamsWithName(all, "\$all", field);
+    if (containedIn != null) queries.add(_buildQueryWithOperatorAndField(containedIn, "\$in", field));
+    if (notContainedIn != null) queries.add(_buildQueryWithOperatorAndField(notContainedIn, "\$nin", field));
+    if (exists != null) queries.add(_buildQueryWithOperatorAndField(exists, "\$exists", field));
+    if (select != null) queries.add(_buildQueryWithOperatorAndField(select, "\$select", field));
+    if (dontSelect != null) queries.add( _buildQueryWithOperatorAndField(dontSelect, "\$dontSelect", field));
+    if (all != null) queries.add(_buildQueryWithOperatorAndField(all, "\$all", field));
 
     // Works
-    if (regEx != null) existsMap = _runThroughQueryParamsWithName(regEx, "\$regex", field);
+    if (regEx != null) queries.add(_buildQueryWithOperatorAndField(regEx, "\$regex", field));
 
     // Doesnt
-    if (text != null) existsMap = _runThroughQueryParamsWithName(text, "\$text", field);
+    if (text != null) queries.add(_buildQueryWithOperatorAndField(text, "\$text", field));
 
     // -- BUILD QUERY USING MAP
-    for(var item in existsMap.entries){
-      query += "{\"${item.key.toString()}\":${item.value}}";
+    for(var item in queries){
+      if (query == QUERY_START) {
+        query += item;
+      } else {
+        query += ",$item";
+      }
     }
 
     // -- ADD LIMITER
     if (limit != 0) query += '?limit=$limit';
     if (skip != 0) query += '?skip=$skip';
+
+    query += QUERY_END;
 
     // -- TEST
     print("QUERY: $query");
@@ -97,7 +105,22 @@ class QueryBuilder <T extends ParseObject> {
     return query;
   }
 
-  Map _runThroughQueryParams(List<dynamic> list, String queryParam) {
+  _buildQueryWithOperatorAndField(List<dynamic> listOfValuesToQuery, String queryOperator, String tableNameToQuery) {
+
+    var queryOperatorAndValueMap = Map();
+    var queryString = "\"$tableNameToQuery\":";
+
+    for (var queryValue in listOfValuesToQuery) {
+      queryOperatorAndValueMap[queryOperator] = queryValue;
+    }
+
+    var formattedQueryOperatorAndValue = JsonEncoder().convert(queryOperatorAndValueMap);
+    queryString += "$formattedQueryOperatorAndValue";
+
+    return queryString;
+  }
+
+  _runThroughQueryParams(List<dynamic> list, String queryParam) {
     Map<String, dynamic> mapToReturn = Map<String, dynamic>();
     var params;
 
@@ -115,26 +138,10 @@ class QueryBuilder <T extends ParseObject> {
 
     mapToReturn[queryParam] = params;
 
-    return mapToReturn;
+    return JsonEncoder().convert(mapToReturn);
   }
 
-  Map<String, String> _runThroughQueryParamsWithName(List<dynamic> list, String queryParam, String fieldName) {
-    Map<String, String> mapToReturn = Map<String, String>();
-    Map<String, dynamic> mapWithParamData = Map<String, dynamic>();
-
-    for (var item in list) {
-      mapWithParamData.putIfAbsent(queryParam, item);
-    }
-
-    var params =  JsonEncoder().convert(mapWithParamData);
-
-    mapToReturn[fieldName] = params;
-
-    return mapToReturn;
-  }
-
-  Map<String, String> _runThroughQueryParamsWithSearchTerms(
-      List<dynamic> list, String queryParam, String fieldName) {
+  Map<String, String> _runThroughQueryParamsWithSearchTerms(List<dynamic> list, String queryParam, String fieldName) {
     Map<String, String> mapToReturn = Map<String, String>();
     Map<String, dynamic> mapWithParamData = Map<String, dynamic>();
     Map<String, String> textEntry = Map<String, String>();
