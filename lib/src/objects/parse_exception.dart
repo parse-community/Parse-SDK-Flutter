47176b4476e499a74c0b705843cacca64a037d95
wigginsp <phillipwiggins@rentalcars.com>
2019-01-09 16:34:41 +0000

    Version 1.0.3 - Amended decoder

--- lib/src/objects/parse_exception.dart
+++ lib/src/objects/parse_exception.dart
@@ -3,6 +3,7 @@ part of flutter_parse_sdk;
 /// ParseException is used in [ParseResult] to inform the user of the exception
 class ParseException {
 
+  int code;
   String message;
 
   ParseException();
