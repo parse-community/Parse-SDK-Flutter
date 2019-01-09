454fbee0665adc5d8f0fb5d6d149336074673737
wigginsp <phillipwiggins@rentalcars.com>
2019-01-09 16:30:51 +0000

    Version 1.0.3 - Added decoder

--- example/lib/application_constants.dart
+++ example/lib/application_constants.dart
@@ -1,6 +1,6 @@
-abstract class ApplicationConstants { // Strart
-  static const String APP_NAME = "BodyCal";
-  static const String PARSE_APPLICATION_ID = "bodycaldb";
-  static const String PARSE_MASTER_KEY = "343gf35g4t6hev445f4t5f45g45d";
-  static const String PARSE_SERVER_URL = "https://bc-server.purewowstudio.com/parse";
+abstract class ApplicationConstants { // Start
+  static const String APP_NAME = "";
+  static const String PARSE_APPLICATION_ID = "";
+  static const String PARSE_MASTER_KEY = "";
+  static const String PARSE_SERVER_URL = "";
 } // End
