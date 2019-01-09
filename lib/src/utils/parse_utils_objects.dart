92f2e4842c996cffcdd7867f2af2dcbc011c7e49
Phill <phill.wiggins@gmail.com>
2019-01-09 21:31:04 +0000

    Version 1.0.3 - Merger loggers

--- lib/src/utils/parse_utils_objects.dart
+++ lib/src/utils/parse_utils_objects.dart
@@ -1,7 +1,7 @@
 part of flutter_parse_sdk;
 
 /// Populates the base object data from a server response
-populateObjectBaseData(ParseObject object, Map<String, dynamic> objectData) {
+populateObjectBaseData(ParseBase object, Map<String, dynamic> objectData) {
   object.set(OBJECT_ID, objectData[OBJECT_ID]);
   object.set(CREATED_AT, stringToDateTime(objectData[CREATED_AT]));
   object.set(OBJECT_ID, stringToDateTime(objectData[UPDATED_AT]));
