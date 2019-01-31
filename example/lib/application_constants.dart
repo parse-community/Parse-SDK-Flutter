9c70784113c26313035f15b424361bdd30362abe
Phill <phill.wiggins@gmail.com>
2019-01-31 21:38:54 +0000

    Corrected app consts

---
+++ example/lib/application_constants.dart
@@ -0,0 +1,6 @@
+abstract class ApplicationConstants {
+  static const String keyAppName = "";
+  static const String keyParseApplicationId = "";
+  static const String keyParseMasterKey = "";
+  static const String keyParseServerUrl = "";
+}
