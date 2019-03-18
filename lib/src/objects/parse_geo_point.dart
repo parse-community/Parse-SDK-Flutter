1977500ec86dec72ead2196e7c5d936594afffff
Phill <phill.wiggins@gmail.com>
2019-03-18 17:04:23 +0000

    Corrected GeoPoints

--- lib/src/objects/parse_geo_point.dart
+++ lib/src/objects/parse_geo_point.dart
@@ -1,16 +1,14 @@
 part of flutter_parse_sdk;
 
 class ParseGeoPoint extends ParseObject {
-
   /// Creates a Parse Object of type GeoPoint
   ParseGeoPoint(
       {this.latitude = 0.0,
-        this.longitude = 0.0,
+      this.longitude = 0.0,
       bool debug,
       ParseHTTPClient client,
       bool autoSendSessionId})
       : super(keyGeoPoint) {
-
     _debug = isDebugEnabled(providedDebugStatus: debug);
     _client = getDefaultHttpClient(client, autoSendSessionId);
   }
@@ -19,7 +17,8 @@ class ParseGeoPoint extends ParseObject {
   double longitude;
 
   @override
-  Map<String, dynamic> toJson({bool full = false, bool forApiRQ = false}) => <String, dynamic>{
+  Map<String, dynamic> toJson({bool full = false, bool forApiRQ = false}) =>
+      <String, dynamic>{
         '__type': 'GeoPoint',
         'latitude': latitude,
         'longitude': longitude
