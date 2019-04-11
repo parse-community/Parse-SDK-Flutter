435941303699a16ab6abb897899e8e4c5cf6c425
rodrigosmarques <rodrigosmarques@gmail.com>
2019-04-11 02:31:33 -0300

    BugFix LiveQuery
    BugFix LiveQuery

--- lib/src/network/parse_live_query.dart
+++ lib/src/network/parse_live_query.dart
@@ -1,52 +1,160 @@
 part of flutter_parse_sdk;
 
-/// Still under development
+enum LiveQueryEvent { create, enter, update, leave, delete, error }
+
 class LiveQuery {
+  LiveQuery({bool debug, ParseHTTPClient client, bool autoSendSessionId}) {
+    _client = client ??
+        ParseHTTPClient(
+            sendSessionId:
+                autoSendSessionId ?? ParseCoreData().autoSendSessionId,
+            securityContext: ParseCoreData().securityContext);
 
-  LiveQuery(ParseHTTPClient client) : client = client {
-    connectMessage = <String, String>{
+    _debug = isDebugEnabled(objectLevelDebug: debug);
+    _sendSessionId = autoSendSessionId ?? ParseCoreData().autoSendSessionId;
+
+    _connectMessage = {
       'op': 'connect',
-      'applicationId': client.data.applicationId,
+      'applicationId': _client.data.applicationId,
+      'clientKey': _client.data.clientKey
     };
-
-    final Map<String, dynamic> whereMap = Map<String, dynamic>();
-
-    subscribeMessage = {
-      'op': 'subscribe',
-      'requestId': 1,
-      'query': {
-        'className': null,
-        'where': whereMap,
-      }
-    };
+    if (_sendSessionId) {
+      _connectMessage['sessionToken'] = _client.data.sessionId;
+    }
   }
 
-  final ParseHTTPClient client;
-  IOWebSocketChannel channel;
-  Map<String, Object> connectMessage;
-  Map<String, Object> subscribeMessage;
+  WebSocket _webSocket;
+  ParseHTTPClient _client;
+  bool _debug;
+  bool _sendSessionId;
+  IOWebSocketChannel _channel;
+  Map<String, dynamic> _connectMessage;
+  Map<String, dynamic> _disconnectMessage;
+  Map<String, dynamic> _subscribeMessage;
   Map<String, Function> eventCallbacks = {};
+  Map<String, dynamic> _whereMap = Map<String, dynamic>();
+  int _requestIdCount = 1;
+  List<String> _liveQueryEvent = [
+    'create',
+    'enter',
+    'update',
+    'leave',
+    'delete',
+    'error'
+  ];
 
-  Future<void> subscribe(String className) async {
-    final WebSocket webSocket = await WebSocket.connect(client.data.liveQueryURL);
-    channel = IOWebSocketChannel(webSocket);
-    channel.sink.add(jsonEncode(connectMessage));
-    final Map<String, dynamic> classNameMap = subscribeMessage['query'];
-    classNameMap['className'] = className;
-    channel.sink.add(jsonEncode(subscribeMessage));
-
-    channel.stream.listen((dynamic message) {
-      final Map<String, dynamic> actionData = jsonDecode(message);
-      if (eventCallbacks.containsKey(actionData['op']))
-        eventCallbacks[actionData['op']](actionData);
-    });
+  int _requestIdGenerator() {
+    return _requestIdCount++;
   }
 
-  void on(String op, Function callback) {
-    eventCallbacks[op] = callback;
+  Future<void> subscribe(QueryBuilder query) async {
+    String _liveQueryURL = _client.data.liveQueryURL;
+    if (_liveQueryURL.contains('https')) {
+      _liveQueryURL = _liveQueryURL.replaceAll('https', 'wws');
+    } else if (_liveQueryURL.contains('http')) {
+      _liveQueryURL = _liveQueryURL.replaceAll('http', 'ww');
+    }
+
+    final String _className = query.object.className;
+    //Remove limites in LiveQuery
+    query.limiters.clear();
+    final String _where = query._buildQuery().replaceAll('where=', '');
+    if (_where != '') {
+      _whereMap = json.decode(_where);
+    }
+
+    final int requestId = _requestIdGenerator();
+
+    try {
+      _webSocket = await WebSocket.connect(
+        _liveQueryURL,
+      );
+
+      if (_webSocket != null && _webSocket.readyState == WebSocket.OPEN) {
+        if (_debug) {
+          print('Livequery: Socket opened');
+        }
+      } else {
+        if (_debug) {
+          print('Livequery: Error when connection client');
+          return;
+        }
+      }
+      _channel = IOWebSocketChannel(_webSocket);
+
+      _channel.stream.listen((dynamic message) {
+        if (_debug) {
+          print('Livequery: Listen: ${message}');
+        }
+
+        final Map<String, dynamic> actionData = jsonDecode(message);
+
+        if (eventCallbacks.containsKey(actionData['op'])) {
+          if (actionData.containsKey('object')) {
+            final Map<String, dynamic> map = actionData['object'];
+            final String className = map['className'];
+            if (className == '_User') {
+              eventCallbacks[actionData['op']](
+                  ParseUser._getEmptyUser().fromJson(map));
+            } else {
+              eventCallbacks[actionData['op']](
+                  ParseObject(className).fromJson(map));
+            }
+          } else {
+            eventCallbacks[actionData['op']](actionData);
+          }
+        }
+      }, onDone: () {
+        if (_debug) {
+          print("Livequery: Task Done");
+        }
+      }, onError: (error) {
+        return handleException(error, ParseApiRQ.liveQuery, _debug, _className);
+      });
+
+      //The connect message is sent from a client to the LiveQuery server.
+      //It should be the first message sent from a client after the WebSocket connection is established.
+      _channel.sink.add(jsonEncode(_connectMessage));
+
+      _subscribeMessage = {
+        'op': 'subscribe',
+        'requestId': requestId,
+        'query': {
+          'className': _className,
+          'where': _whereMap,
+        }
+      };
+      if (_sendSessionId) {
+        _subscribeMessage['sessionToken'] = _client.data.sessionId;
+      }
+
+      //After a client connects to the LiveQuery server,
+      //it can send a subscribe message to subscribe a ParseQuery.
+      _channel.sink.add(jsonEncode(_subscribeMessage));
+
+      _disconnectMessage = {
+        'op': 'unsubscribe',
+        'requestId': requestId,
+      };
+    } on Exception catch (e) {
+      print('Error: ${e.toString()}');
+      return handleException(e, ParseApiRQ.liveQuery, _debug, _className);
+    }
   }
 
-  Future<void> close() async {
-    await channel.sink.close();
+  void on(LiveQueryEvent op, Function callback) {
+    eventCallbacks[_liveQueryEvent[op.index]] = callback;
+  }
+
+  Future<void> unSubscribe() async {
+    if (_channel != null) {
+      if (_channel.sink != null) {
+        await _channel.sink.add(jsonEncode(_disconnectMessage));
+        await _channel.sink.close();
+      }
+    }
+    if (_webSocket != null && _webSocket.readyState == WebSocket.OPEN) {
+      await _webSocket.close();
+    }
   }
 }
