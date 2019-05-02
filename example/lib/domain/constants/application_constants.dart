f07966f6efa461fdaa67457c5113188fad082f19
Ahmed Wahdan <ahmed.wahdan@outlook.com>
2019-05-02 17:44:09 +0200

    Update application_constants.dart

--- example/lib/domain/constants/application_constants.dart
+++ example/lib/domain/constants/application_constants.dart
@@ -1,4 +1,4 @@
-const String keyApplicationName = 'Bind';
-const String keyParseApplicationId = 'qI9WYmrtoqLy0NLtv5LA18AeXMLVfXTfkEowCe5j';
-const String keyParseMasterKey = '3nnUnc6Hz0AE7EdUjIAbURhanZ1DxjX9EVdxIqMH';
-const String keyParseServerUrl = 'https://bind.back4app.io/';
+const String keyApplicationName = '';
+const String keyParseApplicationId = '';
+const String keyParseMasterKey = '';
+const String keyParseServerUrl = '';
