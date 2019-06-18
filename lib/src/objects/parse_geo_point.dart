7c97afb33bcb1a32802e0b6ab155f8770a9da18a
杨萧玉 <yulingtianxia@users.noreply.github.com>
2019-06-19 00:25:57 +0800

    Fix save (#199)
    * Fix Commit: 6ed26e16964f2c316427cde02936a650a76f4159
    
    * Update parse_encoder.dart
    
    Fix parse encoder bug.
    
    * Fix exception when sendSessionId is null.
    
    * Fix GeoPoint.
    
    * delete useless code.

--- lib/src/objects/parse_geo_point.dart
+++ lib/src/objects/parse_geo_point.dart
@@ -3,33 +3,12 @@ part of flutter_parse_sdk;
 const String keyLatitude = 'latitude';
 const String keyLongitude = 'longitude';
 
-class ParseGeoPoint extends ParseObject {
+class ParseGeoPoint {
   /// Creates a Parse Object of type GeoPoint
-  ParseGeoPoint(
-      {double latitude = 0.0,
-      double longitude = 0.0,
-      bool debug,
-      ParseHTTPClient client,
-      bool autoSendSessionId})
-      : super(keyGeoPoint) {
-    this.latitude = latitude;
-    this.longitude = longitude;
+  ParseGeoPoint({this.latitude = 0.0, this.longitude = 0.0});
 
-    _debug = isDebugEnabled(objectLevelDebug: debug);
-    _client = client ??
-        ParseHTTPClient(
-            sendSessionId:
-                autoSendSessionId ?? ParseCoreData().autoSendSessionId,
-            securityContext: ParseCoreData().securityContext);
-  }
+  double latitude, longitude;
 
-  double get latitude => super.get<double>(keyLatitude);
-  set latitude(double latitude) => set<double>(keyLatitude, latitude);
-
-  double get longitude => super.get<double>(keyLongitude);
-  set longitude(double longitude) => set<double>(keyLongitude, longitude);
-
-  @override
   Map<String, dynamic> toJson({bool full = false, bool forApiRQ = false}) =>
       <String, dynamic>{
         '__type': 'GeoPoint',
