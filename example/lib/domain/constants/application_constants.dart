8c7f286cd8536082aede79270b6e12210022ac15
Phill <phill.wiggins@gmail.com>
2019-06-23 16:39:43 +0100

    Merged v1.0.22

--- example/lib/domain/constants/application_constants.dart
+++ example/lib/domain/constants/application_constants.dart
@@ -1,4 +1,4 @@
-const String keyApplicationName = 'BodyCal';
-const String keyParseApplicationId = 'bodycaldb';
-const String keyParseMasterKey = '343gf35g4t6hev445f4t5f45g45d';
-const String keyParseServerUrl = 'http://45.76.129.16:1338/parse/';
+const String keyApplicationName = '';
+const String keyParseApplicationId = '';
+const String keyParseMasterKey = '';
+const String keyParseServerUrl = '';
