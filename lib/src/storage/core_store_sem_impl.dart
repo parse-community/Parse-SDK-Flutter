4032963348a81191651794e55b8a30a79e652614
maaeps <github@maaeps.de>
2020-03-05 23:39:03 +0100

    Fix for Sembast-API change
    See: https://github.com/parse-community/Parse-SDK-Flutter/issues/321#issue-576574198

--- lib/src/storage/core_store_sem_impl.dart
+++ lib/src/storage/core_store_sem_impl.dart
@@ -2,7 +2,7 @@ part of flutter_parse_sdk;
 
 // ignore_for_file: deprecated_member_use
 class CoreStoreSembastImp implements CoreStore {
-  CoreStoreSembastImp._internal(this._store);
+  CoreStoreSembastImp._internal(this._database, this._store);
 
   static CoreStoreSembastImp _instance;
 
@@ -16,86 +16,88 @@ class CoreStoreSembastImp implements CoreStore {
         dbDirectory = (await getApplicationDocumentsDirectory()).path;
       final String dbPath = path.join('$dbDirectory/parse', 'parse.db');
       final Database db = await factory.openDatabase(dbPath, codec: codec);
-      _instance = CoreStoreSembastImp._internal(db);
+      _instance =
+          CoreStoreSembastImp._internal(db, StoreRef<String, String>.main());
     }
 
     return _instance;
   }
 
-  final Database _store;
+  final Database _database;
+  final StoreRef<String, dynamic> _store;
 
   @override
   Future<bool> clear() {
-    return _store.clear();
+    return _store.drop(_database);
   }
 
   @override
   Future<bool> containsKey(String key) {
-    return _store.containsKey(key);
+    return _store.record(key).exists(_database);
   }
 
   @override
   Future<dynamic> get(String key) {
-    return _store.get(key);
+    return _store.record(key).get(_database);
   }
 
   @override
   Future<bool> getBool(String key) async {
-    final bool storedItem = await _store.get(key);
+    final bool storedItem = await get(key);
     return storedItem;
   }
 
   @override
   Future<double> getDouble(String key) async {
-    final double storedItem = await _store.get(key);
+    final double storedItem = await get(key);
     return storedItem;
   }
 
   @override
   Future<int> getInt(String key) async {
-    final int storedItem = await _store.get(key);
+    final int storedItem = await get(key);
     return storedItem;
   }
 
   @override
   Future<String> getString(String key) async {
-    final String storedItem = await _store.get(key);
+    final String storedItem = await get(key);
     return storedItem;
   }
 
   @override
   Future<List<String>> getStringList(String key) async {
-    final List<String> storedItem = await _store.get(key);
+    final List<String> storedItem = await get(key);
     return storedItem;
   }
 
   @override
   Future<void> remove(String key) {
-    return _store.delete(key);
+    return _store.record(key).delete(_database);
   }
 
   @override
   Future<void> setBool(String key, bool value) {
-    return _store.put(value, key);
+    return _store.record(key).put(_database, value);
   }
 
   @override
   Future<void> setDouble(String key, double value) {
-    return _store.put(value, key);
+    return _store.record(key).put(_database, value);
   }
 
   @override
   Future<void> setInt(String key, int value) {
-    return _store.put(value, key);
+    return _store.record(key).put(_database, value);
   }
 
   @override
   Future<void> setString(String key, String value) {
-    return _store.put(value, key);
+    return _store.record(key).put(_database, value);
   }
 
   @override
   Future<void> setStringList(String key, List<String> values) {
-    return _store.put(values, key);
+    return _store.record(key).put(_database, values);
   }
 }
