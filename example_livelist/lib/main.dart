2642cd4a0bee74d9bc21f7c95feb0f803d2a7238
maaeps <github@maaeps.de>
2020-03-10 00:29:36 +0100

    Update main.dart

--- example_livelist/lib/main.dart
+++ example_livelist/lib/main.dart
@@ -38,7 +38,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('LiveList example app'),
         ),
         body: initFailed == false
             ? buildBody(context)
