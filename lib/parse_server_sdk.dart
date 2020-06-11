db10be3fa1c3cf3419ab8f622c1cf75646c4aa3c
Nils Strelow <1096841+nstrelow@users.noreply.github.com>
2020-06-11 22:54:17 +0200

    Make web compatible calls for locale and skip PackageInfo (#395)
    * Make web compatible calls for locale and skip PackageInfo
    
    * Also get country code for web locale
    
    * Add description of early web support

--- lib/parse_server_sdk.dart
+++ lib/parse_server_sdk.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 import 'dart:typed_data';
+import 'dart:ui' as ui;
 
 import 'package:devicelocale/devicelocale.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
