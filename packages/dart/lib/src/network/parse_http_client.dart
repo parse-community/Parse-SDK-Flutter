589aa5a29c249d5f3ac061cc2796096954cc7ba8
fischerscode <github@maaeps.de>
2020-09-17 11:42:39 +0200

    fixing nullpointer

--- packages/dart/lib/src/network/parse_http_client.dart
+++ packages/dart/lib/src/network/parse_http_client.dart
@@ -16,14 +16,14 @@ class ParseHTTPClient with DioMixin implements Dio {
   /// Overrides the call method for HTTP Client and adds custom headers
   @override
   Future<Response<T>> request<T>(
-      String path, {
-        dynamic data,
-        Map<String, dynamic> queryParameters,
-        CancelToken cancelToken,
-        Options options,
-        ProgressCallback onSendProgress,
-        ProgressCallback onReceiveProgress,
-      }) {
+    String path, {
+    dynamic data,
+    Map<String, dynamic> queryParameters,
+    CancelToken cancelToken,
+    Options options,
+    ProgressCallback onSendProgress,
+    ProgressCallback onReceiveProgress,
+  }) {
     options ??= Options();
     if (!identical(0, 0.0)) {
       options.headers[keyHeaderUserAgent] = _userAgent;
@@ -36,7 +36,7 @@ class ParseHTTPClient with DioMixin implements Dio {
 
     if (this.data.clientKey != null)
       options.headers[keyHeaderClientKey] = this.data.clientKey;
-    if (data.masterKey != null)
+    if (this.data.masterKey != null)
       options.headers[keyHeaderMasterKey] = this.data.masterKey;
 
     /// If developer wants to add custom headers, extend this class and add headers needed.
