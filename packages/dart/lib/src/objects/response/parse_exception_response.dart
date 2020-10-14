b3b97c1aedb5b01daed8ac39dc13d59c3709fa4b
Maximilian Fischer <github@maaeps.de>
2020-10-14 17:34:57 +0200

    Dio error response (#482)
    * potential fix #481
    * cloud function add catch
       Fix #484
       Mightfix: #481
    
    Co-authored-by: Chralu <375723+Chralu@users.noreply.github.com>

--- packages/dart/lib/src/objects/response/parse_exception_response.dart
+++ packages/dart/lib/src/objects/response/parse_exception_response.dart
@@ -3,7 +3,24 @@ part of flutter_parse_sdk;
 /// Handles exception instead of throwing an exception
 ParseResponse buildParseResponseWithException(Exception exception) {
   final ParseResponse response = ParseResponse();
-  response.error =
-      ParseError(message: exception.toString(), exception: exception);
+  if (exception is DioError) {
+    try {
+      final Map<String, dynamic> errorResponse =
+          json.decode(exception.response?.data?.toString() ?? '{}');
+
+      response.error = ParseError(
+        message: errorResponse['error']?.toString(),
+        exception: exception,
+        code: errorResponse['code'],
+      );
+    } catch (error) {
+      response.error = ParseError(
+          message: "Failed to build ParseResponse with exception",
+          exception: error);
+    }
+  } else {
+    response.error =
+        ParseError(message: exception.toString(), exception: exception);
+  }
   return response;
 }
