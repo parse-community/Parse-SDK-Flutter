f250b9d62d71828c2e76fac38be4c654e746ed42
Maximilian Fischer <github@maaeps.de>
2020-11-16 14:08:45 +0100

    Fixed error intruduced by merging #508
    This #508 was based on master. As a reult, this happend. Should be fixed now.

--- packages/dart/lib/src/objects/response/parse_exception_response.dart
+++ packages/dart/lib/src/objects/response/parse_exception_response.dart
@@ -3,31 +3,7 @@ part of flutter_parse_sdk;
 /// Handles exception instead of throwing an exception
 ParseResponse buildParseResponseWithException(Exception exception) {
   final ParseResponse response = ParseResponse();
-  if (exception is DioError) {
-    try {
-      Map<String, dynamic> errorResponse;
-
-      try {
-        errorResponse =
-            json.decode(exception.response?.data?.toString() ?? '{}');
-      } catch (_) {}
-
-      errorResponse ??= <String, dynamic>{};
-
-      response.error = ParseError(
-        message: errorResponse['error']?.toString() ??
-            exception.response?.statusMessage,
-        exception: exception,
-        code: errorResponse['code'] ?? exception.response?.statusCode,
-      );
-    } catch (error) {
-      response.error = ParseError(
-          message: "Failed to build ParseResponse with exception",
-          exception: error);
-    }
-  } else {
-    response.error =
-        ParseError(message: exception.toString(), exception: exception);
-  }
+  response.error =
+      ParseError(message: exception.toString(), exception: exception);
   return response;
 }
