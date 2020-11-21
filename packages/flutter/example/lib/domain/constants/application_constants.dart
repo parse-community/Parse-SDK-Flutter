5c157c5320a0d71230b286356a4e5fbaed44884b
Maximilian Fischer <github@maaeps.de>
2020-11-21 18:13:47 +0100

    remove missleading masterkey documentation

--- packages/flutter/example/lib/domain/constants/application_constants.dart
+++ packages/flutter/example/lib/domain/constants/application_constants.dart
@@ -1,5 +1,5 @@
 const String keyApplicationName = '';
 const String keyParseApplicationId = '';
-const String keyParseMasterKey = '';
+const String keyParseClientKey = '';
 const String keyParseServerUrl = '';
 const bool keyDebug = true;
