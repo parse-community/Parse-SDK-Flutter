db0c06970dc688e5bd8a06c01c6249d906965e0b
Danaru <danaru@tuta.io>
2020-12-04 16:53:14 +0100

    Fix unawaited saveInStorage

--- packages/dart/lib/src/objects/parse_installation.dart
+++ packages/dart/lib/src/objects/parse_installation.dart
@@ -103,7 +103,7 @@ class ParseInstallation extends ParseObject {
 
     final ParseResponse parseResponse = await _create();
     if (parseResponse.success && isCurrent) {
-      saveInStorage(keyParseStoreInstallation);
+      await saveInStorage(keyParseStoreInstallation);
     }
     return parseResponse;
   }
@@ -118,7 +118,7 @@ class ParseInstallation extends ParseObject {
     //ParseResponse parseResponse = await super.save();
     final ParseResponse parseResponse = await _save();
     if (parseResponse.success && isCurrent) {
-      saveInStorage(keyParseStoreInstallation);
+      await saveInStorage(keyParseStoreInstallation);
     }
     return parseResponse;
   }
