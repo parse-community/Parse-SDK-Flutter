7368d2b2c2f8872b2a2f7061cd0b90ccaab8e7de
Rodrigo de Souza Marques <rodrigosmarques@gmail.com>
2021-01-14 00:05:25 -0300

    Fix CoreStoreSembastImp / Fix ParseACL / New Query Method
    * Fix CoreStoreSembastImp with no default passwod causes error in Flutter Package
    * Fix ParseACL with no default value in setReadAccess and setWriteAccess
    *Add method excludeKeys: Exclude specific fields from the returned query

--- packages/dart/lib/src/objects/parse_acl.dart
+++ packages/dart/lib/src/objects/parse_acl.dart
@@ -51,7 +51,7 @@ class ParseACL {
   }
 
   ///Set whether the given user id is allowed to read this object.
-  void setReadAccess({@required String userId, bool allowed}) {
+  void setReadAccess({@required String userId, bool allowed = true}) {
     if (userId == null) {
       throw 'cannot setReadAccess for null userId';
     }
@@ -74,7 +74,7 @@ class ParseACL {
   }
 
   ///Set whether the given user id is allowed to write this object.
-  void setWriteAccess({@required String userId, bool allowed}) {
+  void setWriteAccess({@required String userId, bool allowed = true}) {
     if (userId == null) {
       throw 'cannot setWriteAccess for null userId';
     }
