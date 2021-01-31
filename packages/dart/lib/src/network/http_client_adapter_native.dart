a9b02622c0841701f3b47466012747afa90d5573
fengyiyi <iosfeng@163.com>
2021-01-31 19:04:09 +0800

    How to connect Charles Proxy with Flutter using Dio

--- packages/dart/lib/src/network/http_client_adapter_native.dart
+++ packages/dart/lib/src/network/http_client_adapter_native.dart
@@ -4,11 +4,22 @@ import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
 
 HttpClientAdapter createHttpClientAdapter(SecurityContext securityContext) {
-  final DefaultHttpClientAdapter defaultHttpClientAdapter =
-      DefaultHttpClientAdapter();
+  final DefaultHttpClientAdapter defaultHttpClientAdapter = DefaultHttpClientAdapter();
 
-  if (securityContext != null)
-    defaultHttpClientAdapter.onHttpClientCreate =
-        (HttpClient client) => HttpClient(context: securityContext);
+  /// How to connect Charles Proxy with Flutter using Dio
+  /// https://medium.com/netguru/how-to-connect-charles-proxy-with-dio-4443af7bbaa8
+  const charlesIp = String.fromEnvironment('CHARLES_PROXY_IP', defaultValue: null);
+  if (charlesIp != null) {
+    print('#CharlesProxyEnabled');
+    defaultHttpClientAdapter.onHttpClientCreate = (client) {
+      client.findProxy = (uri) => "PROXY $charlesIp:8888;";
+      client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+    };
+    return defaultHttpClientAdapter;
+  }
+
+  if (securityContext != null) {
+    defaultHttpClientAdapter.onHttpClientCreate = (HttpClient client) => HttpClient(context: securityContext);
+  }
   return defaultHttpClientAdapter;
 }
