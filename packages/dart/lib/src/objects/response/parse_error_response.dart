2685fc7980181ba125ee200f994da7d82283f36f
Nils Strelow <strelow.nils@gmail.com>
2021-03-18 15:09:12 +0100

    dart(error_response): Migrate

--- packages/dart/lib/src/objects/response/parse_error_response.dart
+++ packages/dart/lib/src/objects/response/parse_error_response.dart
@@ -3,10 +3,6 @@ part of flutter_parse_sdk;
 /// Handles any errors returned in response
 ParseResponse buildErrorResponse(
     ParseResponse response, ParseNetworkResponse apiResponse) {
-  if (apiResponse.data == null) {
-    return null;
-  }
-
   final Map<String, dynamic> responseData = json.decode(apiResponse.data);
   response.error = ParseError(
       code: responseData[keyCode], message: responseData[keyError].toString());
