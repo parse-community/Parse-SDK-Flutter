87c587697945487c3e5faa113fe05c73d626b6ed
Nils Strelow <strelow.nils@gmail.com>
2021-03-20 17:42:25 +0100

    dart: Fix Web support by migrating dio_adapter_js
    Fixes #588

--- packages/dart/lib/src/network/dio_adapter_js.dart
+++ packages/dart/lib/src/network/dio_adapter_js.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:dio/adapter_browser.dart';
 import 'package:dio/dio.dart';
 
-HttpClientAdapter createHttpClientAdapter(SecurityContext securityContext) {
+HttpClientAdapter createHttpClientAdapter(SecurityContext? securityContext) {
   final BrowserHttpClientAdapter browserHttpClientAdapter =
       BrowserHttpClientAdapter();
   return browserHttpClientAdapter;
