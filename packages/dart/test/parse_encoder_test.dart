0aea5ff89516f35781f6bf4e9d9fd22d8e8af7e7
mbfakourii <mbfakourii@gmail.com>
2022-12-22 16:29:39 +0330

    Solving the problem of tests and reformatting

--- packages/dart/test/parse_encoder_test.dart
+++ packages/dart/test/parse_encoder_test.dart
@@ -1,10 +1,10 @@
+import 'dart:convert';
+
 import 'package:parse_server_sdk/parse_server_sdk.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test(
-      'should return expectedResult json when json has Nested map and list data.',
-      () async {
+  test('should return expectedResult json when json has Nested map and list data.', () async {
     // arrange
     await Parse().initialize(
       'appId',
@@ -20,31 +20,51 @@ void main() {
       appVersion: 'someAppVersion',
     );
 
+    // act
     ParseObject parseObject2 = ParseObject("objectId2");
     parseObject2.objectId = "objectId2";
 
-    // List and Map
-    parseObject2
-        .setAdd("dataParseObjectList", ["ListText1", "ListText2", "ListText3"]);
+    parseObject2.setAdd("dataParseObjectList", ["ListText1", "ListText2", "ListText3"]);
     parseObject2.setAdd("dataParseObjectMap", {
       'KeyTestMap1': 'ValueTestMap1',
       'KeyTestMap2': 'ValueTestMap2',
       'KeyTestMap3': 'ValueTestMap3',
     });
 
-    // parseObject2 inside parseObject1
     ParseObject parseObject1 = ParseObject("parseObject1");
     parseObject1.objectId = "objectId1";
     parseObject1.setAdd("dataParseObject2", parseObject2);
 
-    // desired output
-    String expectedResult =
-        "{className: parseObject1, objectId: objectId1, dataParseObject2: {__op: Add, objects: [{className: objectId2, objectId: objectId2, dataParseObjectList: {__op: Add, objects: [[ListText1, ListText2, ListText3]]}, dataParseObjectMap: {__op: Add, objects: [{KeyTestMap1: ValueTestMap1, KeyTestMap2: ValueTestMap2, KeyTestMap3: ValueTestMap3}]}}]}}";
-
-    // act
     dynamic actualResult = parseEncode(parseObject1, full: true);
 
-    //assert
-    expect(actualResult.toString(), expectedResult);
+    var objectDesiredOutput = {
+      "className": "parseObject1",
+      "objectId": "objectId1",
+      "dataParseObject2": {
+        "__op": "Add",
+        "objects": [
+          {
+            "className": "objectId2",
+            "objectId": "objectId2",
+            "dataParseObjectList": {
+              "__op": "Add",
+              "objects": [
+                ["ListText1", "ListText2", "ListText3"]
+              ],
+            },
+            "dataParseObjectMap": {
+              "__op": "Add",
+              "objects": [
+                {"KeyTestMap1": "ValueTestMap1", "KeyTestMap2": "ValueTestMap2", "KeyTestMap3": "ValueTestMap3"}
+              ]
+            }
+          }
+        ],
+      },
+    };
+    var objectJsonDesiredOutput = jsonEncode(objectDesiredOutput);
+
+    // assert
+    expect(jsonEncode(actualResult), objectJsonDesiredOutput);
   });
 }
--- packages/dart/test/parse_query_test.dart
+++ packages/dart/test/parse_query_test.dart
@@ -2,14 +2,15 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
 import 'package:test/test.dart';
-
+import 'dart:convert';
 import 'parse_query_test.mocks.dart';
 
 @GenerateMocks([ParseClient])
 void main() {
   group('queryBuilder', () {
-    test('whereRelatedTo', () async {
-      final MockParseClient client = MockParseClient();
+    late MockParseClient client;
+    setUp(() async {
+      client = MockParseClient();
 
       await Parse().initialize(
         'appId',
@@ -24,93 +25,113 @@ void main() {
         // to prevent automatic detection
         appVersion: 'someAppVersion',
       );
+    });
 
-      final QueryBuilder<ParseObject> queryBuilder =
-          QueryBuilder<ParseObject>(ParseObject('_User', client: client));
+    test('whereRelatedTo', () async {
+      // arrange
+      final QueryBuilder<ParseObject> queryBuilder = QueryBuilder<ParseObject>(ParseObject('_User', client: client));
       queryBuilder.whereRelatedTo('likes', 'Post', '8TOXdXf3tz');
+
+      var desiredOutput = {
+        "results": [
+          {
+            "objectId": "eT9muOxBTJ",
+            "username": "test",
+            "createdAt": "2021-04-23T13:46:06.092Z",
+            "updatedAt": "2021-04-23T13:46:23.586Z",
+            "ACL": {
+              "*": {"read": true},
+              "eT9muOxBTJ": {"read": true, "write": true}
+            },
+          }
+        ]
+      };
 
       when(client.get(
         any,
         options: anyNamed("options"),
         onReceiveProgress: anyNamed("onReceiveProgress"),
-      )).thenAnswer((_) async => ParseNetworkResponse(
-          statusCode: 200,
-          data:
-              "{\"results\":[{\"objectId\":\"eT9muOxBTJ\",\"username\":\"test\",\"createdAt\":\"2021-04-23T13:46:06.092Z\",\"updatedAt\":\"2021-04-23T13:46:23.586Z\",\"ACL\":{\"*\":{\"read\":true},\"eT9muOxBTJ\":{\"read\":true,\"write\":true}}}]}"));
+      )).thenAnswer((_) async => ParseNetworkResponse(statusCode: 200, data: jsonEncode(desiredOutput)));
 
+      // act
       ParseResponse response = await queryBuilder.query();
 
-      expect(response.results?.first, isA<ParseObject>());
-
       ParseObject parseObject = response.results?.first;
+
+      final Uri result = Uri.parse(verify(client.get(
+        captureAny,
+        options: anyNamed("options"),
+        onReceiveProgress: anyNamed("onReceiveProgress"),
+      )).captured.single);
+
+      var queryDesiredOutput = {
+        "\$relatedTo": {
+          "object": {
+            "__type": "Pointer",
+            "className": "Post",
+            "objectId": "8TOXdXf3tz",
+          },
+          "key": "likes"
+        },
+      };
+      final Uri expectedQuery = Uri(query: 'where=' + jsonEncode(queryDesiredOutput));
+
+      // assert
+      expect(response.results?.first, isA<ParseObject>());
 
       expect(parseObject.get<String>(keyVarUsername), "test");
       expect(parseObject.objectId, "eT9muOxBTJ");
       expect(parseObject.createdAt, DateTime.parse("2021-04-23T13:46:06.092Z"));
       expect(parseObject.updatedAt, DateTime.parse("2021-04-23T13:46:23.586Z"));
 
-      final Uri result = Uri.parse(verify(client.get(
-        captureAny,
-        options: anyNamed("options"),
-        onReceiveProgress: anyNamed("onReceiveProgress"),
-      )).captured.single);
-
       expect(result.path, '/classes/_User');
 
-      final Uri expectedQuery = Uri(
-          query:
-              'where={"\$relatedTo":{"object":{"__type":"Pointer","className":"Post","objectId":"8TOXdXf3tz"},"key":"likes"}}');
       expect(result.query, expectedQuery.query);
     });
 
     test('QueryBuilder.or', () async {
-      final MockParseClient client = MockParseClient();
-
-      await Parse().initialize(
-        'appId',
-        'https://example.com',
-        debug: true,
-        // to prevent automatic detection
-        fileDirectory: 'someDirectory',
-        // to prevent automatic detection
-        appName: 'appName',
-        // to prevent automatic detection
-        appPackageName: 'somePackageName',
-        // to prevent automatic detection
-        appVersion: 'someAppVersion',
-      );
-
+      // arrange
       ParseObject user = ParseObject("_User", client: client);
-      var firstName = QueryBuilder<ParseObject>(user)
-        ..regEx('firstName', "Liam");
+      var firstName = QueryBuilder<ParseObject>(user)..regEx('firstName', "Liam");
 
-      var lastName = QueryBuilder<ParseObject>(user)
-        ..regEx('lastName', "Johnson");
+      var lastName = QueryBuilder<ParseObject>(user)..regEx('lastName', "Johnson");
 
       QueryBuilder<ParseObject> mainQuery = QueryBuilder.or(
         user,
         [firstName, lastName],
       );
 
+      var desiredOutput = {
+        "results": [
+          {
+            "className": "_User",
+            "objectId": "fqx5BECOME",
+            "createdAt": "2022-10-25T06:04:47.138Z",
+            "updatedAt": "2022-10-25T06:05:22.328Z",
+            "firstName": "Liam1",
+            "lastName": "Johnson1",
+          },
+          {
+            "className": "_User",
+            "objectId": "hAtRRYGrUO",
+            "createdAt": "2022-01-24T15:53:48.396Z",
+            "updatedAt": "2022-01-25T05:52:01.701Z",
+            "firstName": "Liam2",
+            "lastName": "Johnson2",
+          }
+        ]
+      };
+
       when(client.get(
         any,
         options: anyNamed("options"),
         onReceiveProgress: anyNamed("onReceiveProgress"),
-      )).thenAnswer((_) async => ParseNetworkResponse(
-          statusCode: 200,
-          data:
-              "{\"results\": [{\"className\": \"_User\",\"objectId\": \"fqx5BECOME\",\"createdAt\": \"2022-10-25T06:04:47.138Z\",\"updatedAt\": \"2022-10-25T06:05:22.328Z\",\"firstName\": \"Liam1\",\"lastName\": \"Johnson1\"},{\"className\": \"_User\",\"objectId\": \"hAtRRYGrUO\",\"createdAt\": \"2022-01-24T15:53:48.396Z\",\"updatedAt\": \"2022-01-25T05:52:01.701Z\",\"firstName\": \"Liam2\",\"lastName\": \"Johnson2\"}]}"));
+      )).thenAnswer((_) async => ParseNetworkResponse(statusCode: 200, data: jsonEncode(desiredOutput)));
 
+      // act
       var response = await mainQuery.query();
 
-      expect(response.results?.first, isA<ParseObject>());
-
       ParseObject parseObject = response.results?.first;
-
-      expect(parseObject.get<String>("firstName"), "Liam1");
-      expect(parseObject.objectId, "fqx5BECOME");
-      expect(parseObject.createdAt, DateTime.parse("2022-10-25T06:04:47.138Z"));
-      expect(parseObject.updatedAt, DateTime.parse("2022-10-25T06:05:22.328Z"));
 
       final Uri result = Uri.parse(verify(client.get(
         captureAny,
@@ -118,34 +139,35 @@ void main() {
         onReceiveProgress: anyNamed("onReceiveProgress"),
       )).captured.single);
 
+      var queryDesiredOutput = {
+        "\$or": [
+          {
+            "firstName": {"\$regex": "Liam"}
+          },
+          {
+            "lastName": {"\$regex": "Johnson"},
+          }
+        ],
+      };
+      final Uri expectedQuery = Uri(query: 'where=' + jsonEncode(queryDesiredOutput));
+
+      // assert
+      expect(response.results?.first, isA<ParseObject>());
+
+      expect(parseObject.get<String>("firstName"), "Liam1");
+      expect(parseObject.objectId, "fqx5BECOME");
+      expect(parseObject.createdAt, DateTime.parse("2022-10-25T06:04:47.138Z"));
+      expect(parseObject.updatedAt, DateTime.parse("2022-10-25T06:05:22.328Z"));
+
       expect(result.path, '/classes/_User');
 
-      final Uri expectedQuery = Uri(
-          query:
-              'where={"\$or":[{"firstName":{"\$regex":"Liam"}},{"lastName":{"\$regex":"Johnson"}}]}');
       expect(result.query, expectedQuery.query);
     });
 
     test('QueryBuilder.and', () async {
-      final MockParseClient client = MockParseClient();
-
-      await Parse().initialize(
-        'appId',
-        'https://example.com',
-        debug: true,
-        // to prevent automatic detection
-        fileDirectory: 'someDirectory',
-        // to prevent automatic detection
-        appName: 'appName',
-        // to prevent automatic detection
-        appPackageName: 'somePackageName',
-        // to prevent automatic detection
-        appVersion: 'someAppVersion',
-      );
-
+      // arrange
       ParseObject user = ParseObject("_User", client: client);
-      var firstName = QueryBuilder<ParseObject>(user)
-        ..regEx('firstName', "jak");
+      var firstName = QueryBuilder<ParseObject>(user)..regEx('firstName', "jak");
 
       var lastName = QueryBuilder<ParseObject>(user)..regEx('lastName', "jaki");
 
@@ -154,88 +176,112 @@ void main() {
         [firstName, lastName],
       );
 
+      var desiredOutput = {
+        "results": [
+          {
+            "className": "_User",
+            "objectId": "fqx5BECOME",
+            "createdAt": "2022-10-25T06:04:47.138Z",
+            "updatedAt": "2022-10-25T06:05:22.328Z",
+            "firstName": "jak1",
+            "lastName": "jaki1",
+          },
+          {
+            "className": "_User",
+            "objectId": "hAtRRYGrUO",
+            "createdAt": "2022-01-24T15:53:48.396Z",
+            "updatedAt": "2022-01-25T05:52:01.701Z",
+            "firstName": "jak2",
+            "lastName": "jaki2",
+          },
+        ]
+      };
+
       when(client.get(
         any,
         options: anyNamed("options"),
         onReceiveProgress: anyNamed("onReceiveProgress"),
-      )).thenAnswer((_) async => ParseNetworkResponse(
-          statusCode: 200,
-          data:
-              "{\"results\": [{\"className\": \"_User\",\"objectId\": \"fqx5BECOME\",\"createdAt\": \"2022-10-25T06:04:47.138Z\",\"updatedAt\": \"2022-10-25T06:05:22.328Z\",\"firstName\": \"jak1\",\"lastName\": \"jaki1\"},{\"className\": \"_User\",\"objectId\": \"hAtRRYGrUO\",\"createdAt\": \"2022-01-24T15:53:48.396Z\",\"updatedAt\": \"2022-01-25T05:52:01.701Z\",\"firstName\": \"jak2\",\"lastName\": \"jaki2\"}]}"));
+      )).thenAnswer((_) async => ParseNetworkResponse(statusCode: 200, data: jsonEncode(desiredOutput)));
 
+      // act
       var response = await mainQuery.query();
 
-      expect(response.results?.first, isA<ParseObject>());
-
       ParseObject parseObject = response.results?.first;
+
+      final Uri result = Uri.parse(verify(client.get(
+        captureAny,
+        options: anyNamed("options"),
+        onReceiveProgress: anyNamed("onReceiveProgress"),
+      )).captured.single);
+
+      var queryDesiredOutput = {
+        "\$and": [
+          {
+            "firstName": {"\$regex": "jak"}
+          },
+          {
+            "lastName": {"\$regex": "jaki"},
+          }
+        ],
+      };
+      final Uri expectedQuery = Uri(query: 'where=' + jsonEncode(queryDesiredOutput));
+
+      // assert
+      expect(response.results?.first, isA<ParseObject>());
 
       expect(parseObject.get<String>("firstName"), "jak1");
       expect(parseObject.objectId, "fqx5BECOME");
       expect(parseObject.createdAt, DateTime.parse("2022-10-25T06:04:47.138Z"));
       expect(parseObject.updatedAt, DateTime.parse("2022-10-25T06:05:22.328Z"));
 
-      final Uri result = Uri.parse(verify(client.get(
-        captureAny,
-        options: anyNamed("options"),
-        onReceiveProgress: anyNamed("onReceiveProgress"),
-      )).captured.single);
-
       expect(result.path, '/classes/_User');
 
-      final Uri expectedQuery = Uri(
-          query:
-              'where={"\$and":[{"firstName":{"\$regex":"jak"}},{"lastName":{"\$regex":"jaki"}}]}');
       expect(result.query, expectedQuery.query);
     });
 
     test('QueryBuilder.nor', () async {
-      final MockParseClient client = MockParseClient();
-
-      await Parse().initialize(
-        'appId',
-        'https://example.com',
-        debug: true,
-        // to prevent automatic detection
-        fileDirectory: 'someDirectory',
-        // to prevent automatic detection
-        appName: 'appName',
-        // to prevent automatic detection
-        appPackageName: 'somePackageName',
-        // to prevent automatic detection
-        appVersion: 'someAppVersion',
-      );
-
+      // arrange
       ParseObject user = ParseObject("_User", client: client);
-      var firstName = QueryBuilder<ParseObject>(user)
-        ..regEx('firstName', "Oliver");
+      var firstName = QueryBuilder<ParseObject>(user)..regEx('firstName', "Oliver");
 
-      var lastName = QueryBuilder<ParseObject>(user)
-        ..regEx('lastName', "Smith");
+      var lastName = QueryBuilder<ParseObject>(user)..regEx('lastName', "Smith");
 
       QueryBuilder<ParseObject> mainQuery = QueryBuilder.nor(
         user,
         [firstName, lastName],
       );
 
+      var desiredOutput = {
+        "results": [
+          {
+            "className": "_User",
+            "objectId": "fqx5BECOME",
+            "createdAt": "2022-10-25T06:04:47.138Z",
+            "updatedAt": "2022-10-25T06:05:22.328Z",
+            "firstName": "Oliver1",
+            "lastName": "Smith1",
+          },
+          {
+            "className": "_User",
+            "objectId": "hAtRRYGrUO",
+            "createdAt": "2022-01-24T15:53:48.396Z",
+            "updatedAt": "2022-01-25T05:52:01.701Z",
+            "firstName": "Oliver2",
+            "lastName": "Smith2",
+          },
+        ]
+      };
+
       when(client.get(
         any,
         options: anyNamed("options"),
         onReceiveProgress: anyNamed("onReceiveProgress"),
-      )).thenAnswer((_) async => ParseNetworkResponse(
-          statusCode: 200,
-          data:
-              "{\"results\": [{\"className\": \"_User\",\"objectId\": \"fqx5BECOME\",\"createdAt\": \"2022-10-25T06:04:47.138Z\",\"updatedAt\": \"2022-10-25T06:05:22.328Z\",\"firstName\": \"Oliver1\",\"lastName\": \"Smith1\"},{\"className\": \"_User\",\"objectId\": \"hAtRRYGrUO\",\"createdAt\": \"2022-01-24T15:53:48.396Z\",\"updatedAt\": \"2022-01-25T05:52:01.701Z\",\"firstName\": \"Oliver2\",\"lastName\": \"Smith2\"}]}"));
+      )).thenAnswer((_) async => ParseNetworkResponse(statusCode: 200, data: jsonEncode(desiredOutput)));
 
+      // act
       var response = await mainQuery.query();
 
-      expect(response.results?.first, isA<ParseObject>());
-
       ParseObject parseObject = response.results?.first;
-
-      expect(parseObject.get<String>("firstName"), "Oliver1");
-      expect(parseObject.objectId, "fqx5BECOME");
-      expect(parseObject.createdAt, DateTime.parse("2022-10-25T06:04:47.138Z"));
-      expect(parseObject.updatedAt, DateTime.parse("2022-10-25T06:05:22.328Z"));
 
       final Uri result = Uri.parse(verify(client.get(
         captureAny,
@@ -243,11 +289,28 @@ void main() {
         onReceiveProgress: anyNamed("onReceiveProgress"),
       )).captured.single);
 
+      var queryDesiredOutput = {
+        "\$nor": [
+          {
+            "firstName": {"\$regex": "Oliver"}
+          },
+          {
+            "lastName": {"\$regex": "Smith"},
+          }
+        ],
+      };
+      final Uri expectedQuery = Uri(query: 'where=' + jsonEncode(queryDesiredOutput));
+
+      // assert
+      expect(response.results?.first, isA<ParseObject>());
+
+      expect(parseObject.get<String>("firstName"), "Oliver1");
+      expect(parseObject.objectId, "fqx5BECOME");
+      expect(parseObject.createdAt, DateTime.parse("2022-10-25T06:04:47.138Z"));
+      expect(parseObject.updatedAt, DateTime.parse("2022-10-25T06:05:22.328Z"));
+
       expect(result.path, '/classes/_User');
 
-      final Uri expectedQuery = Uri(
-          query:
-              'where={"\$nor":[{"firstName":{"\$regex":"Oliver"}},{"lastName":{"\$regex":"Smith"}}]}');
       expect(result.query, expectedQuery.query);
     });
   });
