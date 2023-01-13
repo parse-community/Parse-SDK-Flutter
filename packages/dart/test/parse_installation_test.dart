97a9f6ffb936e61b3842b60acc561a16e6917877
mbfakourii <mbfakourii@gmail.com>
2023-01-13 10:15:20 +0330

    add test

---
+++ packages/dart/test/parse_installation_test.dart
@@ -0,0 +1,33 @@
+import 'package:parse_server_sdk/parse_server_sdk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('should return true for exist TimeZone.', () async {
+    // arrange
+    await Parse().initialize(
+      'appId',
+      'https://example.com',
+      debug: true,
+      // to prevent automatic detection
+      fileDirectory: 'someDirectory',
+      // to prevent automatic detection
+      appName: 'appName',
+      // to prevent automatic detection
+      appPackageName: 'somePackageName',
+      // to prevent automatic detection
+      appVersion: 'someAppVersion',
+    );
+
+    // act
+    final ParseInstallation installation =
+        await ParseInstallation.currentInstallation();
+
+    dynamic actualHasTimeZoneResult = installation.containsKey(keyTimeZone);
+    dynamic actualRealTimeZoneResult =
+        installation[keyTimeZone].toString().contains("/");
+
+    // assert
+    expect(actualHasTimeZoneResult, true);
+    expect(actualRealTimeZoneResult, true);
+  });
+}
