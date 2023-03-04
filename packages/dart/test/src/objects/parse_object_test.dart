31b866766d79b8df6a623c1efcf14ca48f44d7a3
Nidal Bakir <nedalbaker2000@gmail.com>
2023-03-04 07:00:53 +0300

    test: add tests for unset() function

--- packages/dart/test/src/objects/parse_object_test.dart
+++ packages/dart/test/src/objects/parse_object_test.dart
@@ -1281,6 +1281,100 @@ void main() {
           'which is the wrong type. it should be any subtype of num'
           'see the issue #842',
     );
+
+    group('unset()', () {
+      const keyFat = 'fat';
+
+      late ParseObject dietPlansObject;
+
+      setUp(() {
+        dietPlansObject = ParseObject("Diet_Plans", client: client);
+      });
+
+      test('unset() should unset a value from ParseObject locally', () async {
+        // arrange
+        dietPlansObject.set(keyFat, 2);
+
+        // act
+        final ParseResponse parseResponse =
+            await dietPlansObject.unset(keyFat, offlineOnly: true);
+
+        // assert
+        expect(parseResponse.success, isTrue);
+
+        expect(dietPlansObject.get(keyFat), isNull);
+
+        verifyNever(client.put(
+          captureAny,
+          options: anyNamed("options"),
+          data: anyNamed('data'),
+        ));
+
+        verifyZeroInteractions(client);
+      });
+
+      test('unset() should unset a value from ParseObject locally on online',
+          () async {
+        // arrange
+        dietPlansObject.set(keyFat, 2);
+        dietPlansObject.objectId = "O6BHlwV48Z";
+
+        final String putData = '{"$keyFat":{"__op":"Delete"}}';
+        final resultFromServer = {
+          keyVarUpdatedAt: "2023-03-04T03:34:35.076Z",
+        };
+        when(client.put(
+          any,
+          options: anyNamed("options"),
+          data: putData,
+        )).thenAnswer(
+          (realInvocation) async => ParseNetworkResponse(
+            statusCode: 200,
+            data: jsonEncode(resultFromServer),
+          ),
+        );
+
+        // act
+        final ParseResponse parseResponse = await dietPlansObject.unset(keyFat);
+
+        // assert
+        expect(parseResponse.success, isTrue);
+
+        expect(dietPlansObject.get(keyFat), isNull);
+
+        verify(client.put(
+          captureAny,
+          options: anyNamed("options"),
+          data: putData,
+        )).called(1);
+
+        verifyNoMoreInteractions(client);
+      });
+
+      test(
+          'If objectId is null, unset() should unset a value from ParseObject '
+          'locally and not make any call to the server and return success Response',
+          () async {
+        // arrange
+        dietPlansObject.set(keyFat, 2);
+
+        // act
+        final ParseResponse parseResponse = await dietPlansObject.unset(keyFat);
+
+        // assert
+        expect(parseResponse.success, isFalse);
+
+        expect(dietPlansObject.get(keyFat), isNull);
+
+        verifyNever(client.put(
+          captureAny,
+          options: anyNamed("options"),
+          data: anyNamed('data'),
+        ));
+
+        verifyZeroInteractions(client);
+      });
+    });
   });
 }
 
