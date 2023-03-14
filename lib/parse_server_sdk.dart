b6ac0f56da4f5e3311bf366af860dca6fcec3bc2
Nidal Bakir <nedalbaker2000@gmail.com>
2023-03-14 18:31:33 +0300

    fix: add an assertion to make sure a connectivityProvider object is provided when liveQueryUrl is provided

--- lib/parse_server_sdk.dart
+++ lib/parse_server_sdk.dart
@@ -2,9 +2,9 @@ library flutter_parse_sdk;
 
 import 'dart:async';
 import 'dart:convert';
-import 'package:universal_io/io.dart';
 import 'dart:math';
 import 'dart:typed_data';
+
 import 'package:dio/dio.dart';
 import 'package:meta/meta.dart';
 import 'package:mime_type/mime_type.dart';
@@ -14,6 +14,7 @@ import 'package:sembast/sembast_io.dart';
 import 'package:sembast_web/sembast_web.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
+import 'package:universal_io/io.dart';
 import 'package:uuid/uuid.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:xxtea/xxtea.dart';
@@ -25,47 +26,89 @@ export 'src/network/parse_dio_client.dart';
 export 'src/network/parse_http_client.dart';
 
 part 'src/base/parse_constants.dart';
+
 part 'src/data/parse_core_data.dart';
+
 part 'src/data/parse_subclass_handler.dart';
+
 part 'src/enums/parse_enum_api_rq.dart';
+
 part 'src/network/options.dart';
+
 part 'src/network/parse_client.dart';
+
 part 'src/network/parse_connectivity.dart';
+
 part 'src/network/parse_live_query.dart';
+
 part 'src/network/parse_query.dart';
+
 part 'src/objects/parse_acl.dart';
+
 part 'src/objects/parse_base.dart';
+
 part 'src/objects/parse_cloneable.dart';
+
 part 'src/objects/parse_config.dart';
+
 part 'src/objects/parse_error.dart';
+
 part 'src/objects/parse_file.dart';
+
 part 'src/objects/parse_file_base.dart';
+
 part 'src/objects/parse_file_web.dart';
+
 part 'src/objects/parse_function.dart';
+
 part 'src/objects/parse_geo_point.dart';
+
 part 'src/objects/parse_installation.dart';
+
 part 'src/objects/parse_merge.dart';
+
 part 'src/objects/parse_object.dart';
+
 part 'src/objects/parse_relation.dart';
+
 part 'src/objects/parse_response.dart';
+
 part 'src/objects/parse_session.dart';
+
 part 'src/objects/parse_user.dart';
+
 part 'src/objects/response/parse_error_response.dart';
+
 part 'src/objects/response/parse_exception_response.dart';
+
 part 'src/objects/response/parse_response_builder.dart';
+
 part 'src/objects/response/parse_response_utils.dart';
+
 part 'src/objects/response/parse_success_no_results.dart';
+
 part 'src/storage/core_store.dart';
+
 part 'src/storage/core_store_memory.dart';
-part 'src/storage/core_store_sp_impl.dart';
+
 part 'src/storage/core_store_sem_impl.dart';
+
+part 'src/storage/core_store_sp_impl.dart';
+
 part 'src/storage/xxtea_codec.dart';
+
 part 'src/utils/parse_date_format.dart';
+
 part 'src/utils/parse_decoder.dart';
+
 part 'src/utils/parse_encoder.dart';
+
 part 'src/utils/parse_live_list.dart';
+
 part 'src/utils/parse_logger.dart';
+
 part 'src/utils/parse_login_helpers.dart';
+
 part 'src/utils/parse_utils.dart';
 
 class Parse {
@@ -107,6 +150,15 @@ class Parse {
     Stream<void>? appResumedStream,
     ParseClientCreator? clientCreator,
   }) async {
+    assert(() {
+      if (liveQueryUrl != null && connectivityProvider == null) {
+        throw 'Live Query can not operate without connectivity provider.\n'
+            'You need to provide a connectivityProvider to the SDK';
+      }
+
+      return true;
+    }());
+
     final String url = removeTrailingSlash(serverUrl);
 
     await ParseCoreData.init(
@@ -141,8 +193,11 @@ class Parse {
 
   bool hasParseBeenInitialized() => _hasBeenInitialized;
 
-  Future<ParseResponse> healthCheck(
-      {bool? debug, ParseClient? client, bool? sendSessionIdByDefault}) async {
+  Future<ParseResponse> healthCheck({
+    bool? debug,
+    ParseClient? client,
+    bool? sendSessionIdByDefault,
+  }) async {
     final bool debugLocal = isDebugEnabled(objectLevelDebug: debug);
 
     final ParseClient clientLocal = client ??
