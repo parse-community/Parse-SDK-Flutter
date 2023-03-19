5400cff836d5cfcc416d16eb7fadc49c38bac24c
Nidal Bakir <nedalbaker2000@gmail.com>
2023-03-19 18:08:07 +0300

    test: add tests for handleResponse function when batch request

--- packages/dart/test/src/objects/response/parse_response_utils_test.dart
+++ packages/dart/test/src/objects/response/parse_response_utils_test.dart
@@ -83,6 +83,82 @@ void main() {
           equals(object2ResultFromServerUpdatedAt),
         );
       });
+
+      test(
+          'should return a ParseResponse holds a list of ParseObject and ParseError',
+          () {
+        // arrange
+        final object1 = ParseObject("object1");
+        final object2 = ParseObject("object2");
+
+        final objectsToBatch = [object1, object2];
+
+        const object1ResultFromServerObjectId = "YAfSAWwXbL";
+        const object1ResultFromServerCreatedAt = "2023-03-19T00:20:37.187Z";
+
+        final resultFromServerForBatch = json.encode(
+          [
+            {
+              "success": {
+                keyVarObjectId: object1ResultFromServerObjectId,
+                keyVarCreatedAt: object1ResultFromServerCreatedAt,
+              }
+            },
+            // error while saving the second object
+            {
+              "error": {
+                "code": ParseError.internalServerError,
+                "error": "internal server error",
+              }
+            }
+          ],
+        );
+
+        final result = ParseNetworkResponse(
+          data: resultFromServerForBatch,
+          statusCode: 200,
+        );
+
+        // act
+        final response = handleResponse(
+          objectsToBatch,
+          result,
+          ParseApiRQ.batch,
+          true,
+          'test_batch',
+        );
+
+        // assert
+        expect(response.success, isTrue);
+
+        expect(response.count, equals(2));
+
+        expect(response.error, isNull);
+
+        expect(response.results, isNotNull);
+
+        final resultsList = response.results!;
+
+        expect(resultsList.length, equals(2));
+
+        final ParseObject firstObject = resultsList[0];
+
+        final ParseError secondObjectError = resultsList[1];
+
+        expect(firstObject.objectId, equals(object1ResultFromServerObjectId));
+
+        expect(
+          firstObject.createdAt!.toIso8601String(),
+          equals(object1ResultFromServerCreatedAt),
+        );
+
+        expect(
+          secondObjectError.code,
+          equals(ParseError.internalServerError),
+        );
+
+        expect(object2.objectId, isNull);
+      });
     });
   });
 }
