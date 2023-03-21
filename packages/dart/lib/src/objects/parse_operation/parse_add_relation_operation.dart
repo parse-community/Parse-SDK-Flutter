e2c683846bc33fe3fd9425dc245aeedf849e852c
Nidal Bakir <nedalbaker2000@gmail.com>
2023-03-21 23:15:06 +0300

    create add/remove relation operation class

---
+++ packages/dart/lib/src/objects/parse_operation/parse_add_relation_operation.dart
@@ -0,0 +1,34 @@
+part of flutter_parse_sdk;
+
+class _ParseAddRelationOperation extends _ParseRelationOperation {
+  _ParseAddRelationOperation(List<ParseObject> value) : super(value);
+
+  @override
+  String get operationName => 'AddRelation';
+
+  @override
+  bool canMergeWith(Object other) {
+    return other is _ParseAddRelationOperation;
+  }
+
+  @override
+  _ParseOperation<List<ParseObject>> mergeWithPrevious(Object previous) {
+    if (!canMergeWith(previous)) {
+      throw _UnmergeableOperationException(this, previous);
+    }
+
+    final List<ParseObject> previousValue;
+
+    if (previous is List<ParseObject>) {
+      previousValue = previous;
+    } else {
+      previousValue =
+          (previous as _ParseAddOperation).value as List<ParseObject>;
+    }
+
+    previousValue.addAll(value);
+    value = previousValue.toSet().toList();
+
+    return this;
+  }
+}
---
+++ packages/dart/lib/src/objects/parse_operation/parse_remove_relation_operation.dart
@@ -0,0 +1,1 @@
+part of flutter_parse_sdk;
