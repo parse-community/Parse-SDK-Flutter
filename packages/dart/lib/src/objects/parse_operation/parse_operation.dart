d95f40f9158f4606c2a4b57499808aa007b32a7f
Nidal Bakir <nedalbaker2000@gmail.com>
2023-04-11 22:11:53 +0300

    docs: add documentation for _ParseOperation abstract class

--- packages/dart/lib/src/objects/parse_operation/parse_operation.dart
+++ packages/dart/lib/src/objects/parse_operation/parse_operation.dart
@@ -1,20 +1,53 @@
 part of flutter_parse_sdk;
 
+/// Represents an operation performed on Parse data. It defines the core
+/// functionality of any operation performed on Parse data.
 abstract class _ParseOperation<T> implements _Valuable<T> {
+  /// Used to store the estimated value after an operation.
+  ///
+  /// This is what the user will see as the result of any operation on the data.
+  /// For example, if the operation is an array addition and the user wants
+  /// to add the value 4 to the list, and the list originally looks like this:
+  /// [1,2,3], then the [value] variable will initially hold [1,2,3].
+  /// After the addition operation is performed, the [value] variable will
+  /// hold [1,2,3,4], which is what the user will see.
+  /// The add operation itself will be stored in a separate variable,
+  /// [valueForApiRequest], which will only hold the data that needs
+  /// to be sent to the server, in this case [4].
   T value;
 
+  /// The actual that will be sent to the server.
   late T valueForApiRequest;
 
   _ParseOperation(this.value);
 
+  /// The name of the preformed operation.
+  ///
+  /// This value can be used when sending the operation to the server.
+  ///
+  /// e.g: Add, AddUnique, Remove, Increment, AddRelation, RemoveRelation
   String get operationName;
 
+  /// Checks if [other] can be merged with the current operation.
+  ///
+  /// Some operations can be merged with others, and each operation defines
+  /// what can be merged. For example, an Add operation can be merged with
+  /// another Add operation or with a [_ParseArray] object.
   bool canMergeWith(Object other);
 
+  /// Preform the merge between [previous] and current operation.
+  ///
+  /// This should be called after [canMergeWith] to check if the [previous]
+  /// operation is eligible to merge with the current operation
+  ///
+  /// Will return the current(this) operation merged with the other(previous)
+  /// operation
   _ParseOperation<T> merge(Object previous);
 
-  Map<String, dynamic> toJson({bool full = false});
-
+  /// Merges the current operation with the [previous] operation if possible.
+  ///
+  /// Throws a [_UnmergeableOperationException] if the [previous] operation
+  /// cannot be merged with the current operation.
   _ParseOperation<T> mergeWithPrevious(Object previous) {
     if (!canMergeWith(previous)) {
       throw _UnmergeableOperationException(this, previous);
@@ -23,14 +56,30 @@ abstract class _ParseOperation<T> implements _Valuable<T> {
     return merge(previous);
   }
 
+  /// Convert the operation to json format (Map).
+  ///
+  /// Will be used to be sent the operation to the server or to store the
+  /// operation in the cache. When [full] is true that should indicate that
+  /// the intention of converting to json is to store the operation
+  /// in the local cache
+  Map<String, dynamic> toJson({bool full = false});
+
+  /// construct a new value of [newValue] to be used in parse object.
+  ///
+  /// * If the [newValue] is [Iterable] will return [_ParseArray]
+  /// * If the [newValue] is [num] will return [_ParseNumber]
+  /// * If the [newValue] is [_ParseOperation] will try to merge the this
+  /// operation with the [previousValue] and return this operation merged
+  /// with the [previousValue] if possible.
+  /// * Otherwise will return the [newValue] as it is.
   static Object? maybeMergeWithPrevious<R>({
     required R newValue,
     required Object? previousValue,
     required ParseObject parent,
     required String key,
   }) {
-    if (newValue is List) {
-      return _ParseArray(setMode: true)..estimatedArray = newValue;
+    if (newValue is Iterable) {
+      return _ParseArray(setMode: true)..estimatedArray = newValue.toList();
     }
 
     if (newValue is num) {
@@ -76,11 +125,11 @@ abstract class _ParseOperation<T> implements _Valuable<T> {
 
     if (previousValue == null) {
       return _ParseNumber(0).preformNumberOperation(numberOperation);
-    } else {
-      throw ParseOperationException(
-          'wrong key, unable to preform numeric operation on'
-          ' the previous value: ${previousValue.runtimeType}');
     }
+
+    throw ParseOperationException(
+        'wrong key, unable to preform numeric operation on'
+        ' the previous value: ${previousValue.runtimeType}');
   }
 
   static _ParseArray _handelArrayOperation(
@@ -93,11 +142,11 @@ abstract class _ParseOperation<T> implements _Valuable<T> {
 
     if (previousValue == null) {
       return _ParseArray().preformArrayOperation(arrayOperation);
-    } else {
-      throw ParseOperationException(
-          'wrong key, unable to preform Array operation on'
-          ' the previous value: ${previousValue.runtimeType}');
     }
+
+    throw ParseOperationException(
+        'wrong key, unable to preform Array operation on'
+        ' the previous value: ${previousValue.runtimeType}');
   }
 
   static _ParseRelation _handelRelationOperation(
@@ -113,13 +162,14 @@ abstract class _ParseOperation<T> implements _Valuable<T> {
     if (previousValue == null) {
       return _ParseRelation(parent: parent, key: key)
           .preformRelationOperation(relationOperation);
-    } else {
-      throw ParseOperationException(
-          'wrong key, unable to preform Relation operation on'
-          ' the previous value: ${previousValue.runtimeType}');
     }
+
+    throw ParseOperationException(
+        'wrong key, unable to preform Relation operation on'
+        ' the previous value: ${previousValue.runtimeType}');
   }
 
+  /// Returns the estimated value of this operation.
   @override
   T getValue() {
     if (value is Iterable) {
