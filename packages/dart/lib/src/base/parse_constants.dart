7913cbd3d3e5ad9337aab661d00188703c3993b2
Nidal Bakir <nidal.bakir.edu@gmail.com>
2023-05-14 03:29:48 +0300

    bump the sdk version to 5.1.0

--- packages/dart/lib/src/base/parse_constants.dart
+++ packages/dart/lib/src/base/parse_constants.dart
@@ -1,7 +1,7 @@
 part of flutter_parse_sdk;
 
 // Library
-const String keySdkVersion = '5.0.1';
+const String keySdkVersion = '5.1.0';
 const String keyLibraryName = 'Flutter Parse SDK';
 
 // End Points
