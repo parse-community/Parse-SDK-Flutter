28efe0975d36217a5828becc619f2907ea7e0b6a
mbfakourii <mbfakourii@gmail.com>
2023-05-14 12:01:29 +0430

    add logo increase range sdk to 4 add example

---
+++ packages/dart/example/main.dart
@@ -0,0 +1,26 @@
+import '../lib/parse_server_sdk.dart';
+
+Future<void> main() async {
+  // Parse initialize
+  await Parse().initialize(
+      "keyApplicationId",
+      "keyParseServerUrl",
+      clientKey: "keyParseClientKey",
+      debug: true,
+      liveQueryUrl: "keyLiveQueryUrl",
+      autoSendSessionId: true,
+      coreStore: CoreStoreMemoryImp());
+
+
+  // Set a ParseObject and save it
+  var dietPlan = ParseObject('DietPlan')
+    ..set('Name', 'Ketogenic')
+    ..set('Fat', 65);
+
+  var response = await dietPlan.save();
+
+  if (response.success) {
+    dietPlan = response.results?.first;
+    print("Response received successfully");
+  }
+}
