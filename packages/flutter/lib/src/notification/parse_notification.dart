a836d8e2d664142eaf99ea8f004db2f582679340
Mohammad Bagher Fakouri <mbfakourii@gmail.com>
2023-05-22 23:42:32 +0330

    feat: Add support for push notifications via `ParsePush`, `ParseNotification`  (#914)

---
+++ packages/flutter/lib/src/notification/parse_notification.dart
@@ -0,0 +1,33 @@
+part of flutter_parse_sdk_flutter;
+
+class ParseNotification {
+  static final ParseNotification instance = ParseNotification._internal();
+  static String keyNotificationChannelName = "parse";
+
+  factory ParseNotification() {
+    return instance;
+  }
+
+  ParseNotification._internal() {
+    // Initialize notifications helper package
+    AwesomeNotifications().initialize(
+      null,
+      [
+        NotificationChannel(
+            channelKey: keyNotificationChannelName,
+            channelName: keyNotificationChannelName,
+            channelDescription: 'Notification channel for parse')
+      ],
+    );
+  }
+
+  /// Show notification
+  void showNotification(title) {
+    AwesomeNotifications().createNotification(
+        content: NotificationContent(
+      id: Random().nextInt(1000),
+      channelKey: keyNotificationChannelName,
+      title: title,
+    ));
+  }
+}
