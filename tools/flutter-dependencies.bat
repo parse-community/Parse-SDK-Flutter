4e4bfabf07684a1e2e9fb911c2c605edb7aacd74
Mohammad Bagher Fakouri <mbfakourii@gmail.com>
2023-05-28 23:55:06 +0330

    refactor: Update `README.md` and improved repository (#926)

--- tools/flutter-dependencies.bat
+++
@@ -1,8 +0,0 @@
-call flutter config --no-analytics
-cd packages/dart
-call flutter pub get
-cd ../..
-cd packages/flutter
-call flutter pub remove parse_server_sdk
-call flutter pub add parse_server_sdk --path ../dart
-call flutter pub get
