6669241c282949e94b52f62a45ce7387b629083e
mbfakourii <mbfakourii@gmail.com>
2023-07-10 08:34:20 +0330

    revert

--- packages/flutter/example/linux/flutter/generated_plugins.cmake
+++ packages/flutter/example/linux/flutter/generated_plugins.cmake
@@ -3,11 +3,11 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-        awesome_notifications
+  awesome_notifications
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-        )
+)
 
 set(PLUGIN_BUNDLED_LIBRARIES)
 
