196566de9a8dfca33f81b094354e1f828335bb41
mbfakourii <mbfakourii@gmail.com>
2025-04-19 14:47:16 +0330

    feat: Use `package:web` instead of `dart:html` for web support (#1036)

--- packages/dart/lib/src/network/parse_websocket_html.dart
+++ packages/dart/lib/src/network/parse_websocket_html.dart
@@ -1,7 +1,7 @@
 /// If you change this file, you should apply the same changes to the 'parse_websocket_io.dart' file
 library;
 
-import 'dart:html' as html;
+import 'package:web/web.dart' as web;
 
 import 'package:web_socket_channel/html.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
@@ -14,10 +14,10 @@ class WebSocket {
   static const int closing = 2;
   static const int closed = 3;
 
-  final html.WebSocket _webSocket;
+  final web.WebSocket _webSocket;
 
   static Future<WebSocket> connect(String liveQueryURL) async {
-    final html.WebSocket webSocket = html.WebSocket(liveQueryURL);
+    final web.WebSocket webSocket = web.WebSocket(liveQueryURL);
     await webSocket.onOpen.first;
     return WebSocket._(webSocket);
   }
