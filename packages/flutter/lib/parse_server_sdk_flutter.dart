e61f29408185b843f11806783f32f552e59237bc
pastordee <davidfrimpong@me.com>
2025-05-01 05:59:02 +0100

    Update parse_server_sdk_flutter.dart

--- packages/flutter/lib/parse_server_sdk_flutter.dart
+++ packages/flutter/lib/parse_server_sdk_flutter.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:ui';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
+import 'package:parse_server_sdk_flutter/src/utils/parse_cached_live_list.dart';
 import 'package:path/path.dart' as path;
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/foundation.dart';
