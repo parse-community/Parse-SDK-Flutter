c60801e80acc1e1a28d3b20a29aaa0bf73af60a7
pastordee <davidfrimpong@me.com>
2025-05-02 09:33:12 +0100

    Update parse_live_list.dart

--- packages/flutter/lib/src/utils/parse_live_list.dart
+++ packages/flutter/lib/src/utils/parse_live_list.dart
@@ -210,8 +210,7 @@ class _ParseLiveListWidgetState<T extends sdk.ParseObject>
         } else if (event is sdk.ParseLiveListDeleteEvent<sdk.ParseObject>) {
           setState(() {
             _items.removeAt(event.index);
-            event.index.removeFromLocalCache();
-            // Optionally: remove from offline cache if you implement a remove method
+            (event.object as T).removeFromLocalCache();
           });
         } else if (event is sdk.ParseLiveListUpdateEvent<sdk.ParseObject>) {
           setState(() {
