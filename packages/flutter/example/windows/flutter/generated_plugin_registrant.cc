4b25476795584784440fd94a0e6498d73bfb2901
pastordee <davidfrimpong@me.com>
2025-08-17 21:41:54 +0100

    Update Parse SDK source and remove connectivity_plus plugin
    Switched parse_server_sdk dependency to use a specific git repository and branch in pubspec.yaml files. Commented out parse_offline_extension and removed connectivity_plus plugin registration from Windows build files.

--- packages/flutter/example/windows/flutter/generated_plugin_registrant.cc
+++ packages/flutter/example/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <connectivity_plus/connectivity_plus_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  ConnectivityPlusWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
 }
--- packages/flutter/example/windows/flutter/generated_plugins.cmake
+++ packages/flutter/example/windows/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  connectivity_plus
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
