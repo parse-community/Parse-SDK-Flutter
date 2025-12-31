18ea019c2a9521791fdde6078cd754562bf488a5
Mehmet Şeref Kayalı <mskayali@yandex.com>
2025-12-31 08:23:38 +0300

    no message

--- packages/dart/lib/src/network/parse_websocket_html.dart
+++ packages/dart/lib/src/network/parse_websocket_html.dart
@@ -1,8 +1,10 @@
 /// If you change this file, you should apply the same changes to the 'parse_websocket_io.dart' file
 library;
 
-import 'dart:html' as html;
 
+import 'dart:async';
+
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:web_socket_channel/html.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
@@ -14,18 +16,41 @@ class WebSocket {
   static const int closing = 2;
   static const int closed = 3;
 
-  final html.WebSocket _webSocket;
+  final IO.Socket _webSocket;
 
+  static final Map<String, int> _states={
+    'closed':3,
+    'opening':0,
+    'open':1,
+    'closing':2
+  };
   static Future<WebSocket> connect(String liveQueryURL) async {
-    final html.WebSocket webSocket = html.WebSocket(liveQueryURL);
-    await webSocket.onOpen.first;
-    return WebSocket._(webSocket);
+    Completer<WebSocket> completer= Completer();
+    final IO.Socket webSocket = IO.io(
+      liveQueryURL,
+      IO.OptionBuilder().setTransports(['websocket']).enableReconnection().build()
+    );
+    webSocket.connect();
+    webSocket.onConnect((handler){
+      if(!completer.isCompleted){
+        completer.complete(WebSocket._(webSocket));
+      }
+    });
+    webSocket.onConnectError((handler){
+      if(!completer.isCompleted){
+        completer.completeError('unable to connect to the server $handler');
+      }
+    });
+
+    
+
+    return completer.future;
   }
 
-  int get readyState => _webSocket.readyState;
+  int get readyState => _states[_webSocket.io.readyState]!;
 
-  Future<void> close() async {
-    return _webSocket.close();
+  Future<IO.Socket> close() async {
+    return _webSocket.disconnect();
   }
 
   WebSocketChannel createWebSocketChannel() {
